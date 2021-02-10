dafe5cb6abf3fa70cac4dca1be1a6d18f5131b72
youwenkai <superceil_you@163.com>
2021-02-10 13:22:07 +0800

    单链表及leetcode的题目

--- src/algorithm/main.java
+++ src/algorithm/main.java
@@ -1,23 +1,39 @@
 package algorithm;
 
-import algorithm.List.ArrayList;
+//import algorithm.List.ArrayList;
+import algorithm.List.SingleLinkedList;
 
 public class main {
 
 	public static void main(String[] args) {
 
-		ArrayList<Object> list  = new ArrayList<>();
+//		ArrayList<Object> list  = new ArrayList<>();
+//		list.add(new Person(10, "Jack"));
+//		list.add(null);
+//		list.add(new Person(15, "Rose"));
+//		list.add(null);
+//		list.add(new Person(12, "James"));
+//		list.add(null);
+//
+//		System.out.println(list.indexOf(null));
+		
+		SingleLinkedList<Object> list = new SingleLinkedList<>();
+		
 		list.add(new Person(10, "Jack"));
 		list.add(null);
 		list.add(new Person(15, "Rose"));
 		list.add(null);
 		list.add(new Person(12, "James"));
 		list.add(null);
+		
+		
+		System.out.println(list);
+		
+//		list.remove(3);
+		
+//		System.out.println(list.set(0, null));
 
-		System.out.println(list.indexOf(null));
-		
-		
-		
+		System.out.println(list.get(list.size() -  1));
 		System.out.println(list);
 	}
 
---
+++ src/leetcode/链表/_237_删除链表中的节点.java
@@ -0,0 +1,28 @@
+package leetcode.链表;
+
+/**
+ * 
+ *  输入：head = [4,5,1,9], node = 5
+    输出：[4,1,9]
+    解释：给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+   
+   	链表至少包含两个节点。
+	链表中所有节点的值都是唯一的。
+	给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+	不要从你的函数中返回任何结果。
+ * */
+
+public class _237_删除链表中的节点 {
+	
+	public void deleteNode(ListNode node) {
+        // 因为这个节点是要被删除的 所以说直接把后面的的节点内容覆盖这个节点节点就行了
+		node.val = node.next.val;
+		node.next = node.next.next;
+    }
+	
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+}
